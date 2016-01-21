166f24dcac4c712590caa1f68045eb503a9e6f0a
liangjiapei1103 <liang98@purdue.edu>
2016-01-21 03:27:03 -0500

    fix the bug, eventname --> eventName

--- app.js
+++ app.js
@@ -1,33 +1,32 @@
-var express = require('express');
-var app = express();
-var path = require('path');
+var express = require('express')
+var app = express()
+var path = require('path')
 
-var port = process.env.PORT || 3000;
+var port = process.env.PORT || 3000
 
 // put static file on static folder
-app.use(express.static(path.join(__dirname, '/static')));
+app.use(express.static(path.join(__dirname, '/static')))
 // index.html will be the initial web page
 app.use(function (req, res) {
 	res.sendFile(path.join(__dirname, './static/index.html'));
-});
+})
 
 var server = app.listen(port, function() {
-	console.log('technode is on port ' + port + '!');
-});
+	console.log('Technode is on port ' + port + '!')
+})
 
-var io = require('socket.io').listen(server);
+var io = require('socket.io').listen(server)
 
-var messages = [];
+var messages = []
 
 io.sockets.on('connection', function(socket) {
 	// when making connection, emit  get all message to get all messages, save messages into messages array
-	socket.on('getAllMessages', function() {
-		socket.emit('allMessages', messages);
+	socket.on('messages.read', function() {
+		socket.emit('messages.read.', messages);
 	});
 
-	socket.on('createMessage', function(message) {
+	socket.on('messages.create', function(message) {
 		messages.push(message);
-		io.sockets.emit('messageAdded', message);
-	});
-	
+		io.sockets.emit('messages.add', message)
+	})
 })
--- static/technode.js
+++ static/technode.js
@@ -1,78 +1,96 @@
-angular.module('technodeApp', []);
+angular.module('techNodeApp', [])
 
-angular.module('technodeApp').factory('socket', function($rootScope) {
-	var socket = io.connect('/');
+angular.module('techNodeApp').factory('socket', function($rootScope) {
+	// var socket = io.connect('/')
+  	//为了避免跨域请求，需要将原来的var socket = io.connect('/')改成下面这一行
+  	// var socket = io.connect('http://localhost:3000/')
+  	//或者采用socket官方主页上面的方法 var socket = io()
+  	var socket = io();
 	return {
 		on: function (eventName, callback) {
 			socket.on(eventName, function() {
-				var args = arguments;
+				var args = arguments
 				// check the whole app's data status, when something changed, update the information in index.html
 				$rootScope.$apply(function() {
-					callback.apply(socket, args);
-				});
-			});
+					callback.apply(socket, args)
+				})
+			})
 		},
 		emit: function (eventName, data, callback) {
-			socket.emit(eventname, data, function() {
-				var args = arguments;
+			socket.emit(eventName, data, function() {
+				var args = arguments
 				$rootScope.$apply(function() {
 					if (callback) {
 						callback.apply(socket, args);
 					}
-				});
-			});
+				})
+			})
 		}
-	};
-});
+	}
+})
 
-angular.module('technodeApp').controller('RoomCtrl', function($scope, socket) {
-	$scope.messages = [];
-
+angular.module('techNodeApp').controller('RoomCtrl', function($scope, socket) {
+	$scope.messages = []
 	// update messages into html
-	socket.emit('getAllMessages');
-	socket.on('allMessages', function(messages) {
+	socket.on('messages.read', function (messages) {
 		$scope.messages = messages;
-	});
+	})
 
 	// listen the messageAdded event on server, when there is new message, add it to html
-	socket.on('messagesAdded', function(message) {
-		$scope.messages.push(message);
+	socket.on('messages.add', function (message) {
+		$scope.messages.push(message)
 	});
-});
+	socket.emit('messages.read')
+})
 
 // after user pressed Enter, the message is sent to the server via Socket
-angular.module('technodeApp').controller('MessageCreatorCtrl', function($scope, socket) {
-	$scope.newMessage = '';
+angular.module('techNodeApp').controller('MessageCreatorCtrl', function($scope, socket) {
+	// $scope.newMessage = '';
 	$scope.createMessage = function() {
 		// do not send empty message
 		if ($scope.newMessage == '')
 			return
 
-		socket.emit('createMessage', $scope.newMessage);
+		socket.emit('messages.create', $scope.newMessage)
 		// after sending the message, set it back to empty
-		$scope.newMessage = '';
+		$scope.newMessage = ''
 	};
 });
 
-angular.module('technodeApp').directive('ctrlEnterBreakLine', function() {
+angular.module('techNodeApp').directive('autoScrollToBottom', function() {
+	return {
+		link: function(scope, element, attrs) {
+			scope.$watch(function() {
+				return element.children().length;
+			},
+			function() {
+				element.animate({
+					scrollTop: element.prop('scrollHeight')
+				}, 1000);
+			});
+		}
+	};
+});
+
+angular.module('techNodeApp').directive('ctrlEnterBreakLine', function() {
 	return function (scope, element, attrs) {
-		var ctrlDown = false;
+		var ctrlDown = false
 		element.bind("keydown", function(evt) {
 			if (evt.which === 17) {
-				ctrlDown = true;
+				ctrlDown = true
 				setTimeout(function() {
-					ctrlDown = false;
-				}, 1000);
+					ctrlDown = false
+				}, 1000)
 			}
 
 			if (evt.which === 13) {
 				if (ctrlDown) {
-					element.val(element.val() + '\n');
+					element.val(element.val() + '\n')
 				} else {
 					scope.$apply(function() {
 						scope.$eval(attrs.ctrlEnterBreakLine);
 					});
-					evt.preventDefault();
+					evt.preventDefault()
 				}
 			}
 		});
