f931f22b3fc0c194c44c005dfd4e2a3500ce73e9
liangjiapei1103 <liang98@purdue.edu>
2016-01-22 16:36:27 -0500

    add user.js and index.js under root/models

--- static/technode.js
+++ static/technode.js
@@ -1,1 +1,15 @@
-angular.module('techNodeApp', ['ngRoute'])
+angular.module('techNodeApp', ['ngRoute']).run(function($window, $rootScope, $http, $location) {
+	$http({
+		// use Ajax to validate user's info
+		url: '/api/validate',
+		method: 'GET'
+		// if success, go to chatting page
+	}).success(function(user) {
+		$rootScope.me = user
+		$location.path('/')
+		// if fail, go to login page
+	}).error(function(data) {
+		$location.path('/login')
+	})
+})
+
