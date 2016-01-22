0f1ef2fd338828a7687e28e399146d6e76a2f4a4
liangjiapei1103 <liang98@purdue.edu>
2016-01-22 18:37:21 -0500

    add logout and stay logged in fuction

--- static/technode.js
+++ static/technode.js
@@ -11,5 +11,17 @@ angular.module('techNodeApp', ['ngRoute']).run(function($window, $rootScope, $ht
 	}).error(function(data) {
 		$location.path('/login')
 	})
+	$rootScope.logout = function() {
+		$http({
+			url: '/ajax/logout',
+			method: 'GET'
+		}).success(function() {
+			$rootScope.me = null
+			$location.path('/login')
+		})
+	}
+	$rootScope.$on('login', function(evt, me) {
+		$rootScope.me = me
+	})
 })
 
