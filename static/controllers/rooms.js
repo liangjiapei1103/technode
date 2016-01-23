5cdbe1fd9374e1d5893c2920ff94b791ac85be38
liangjiapei1103 <liang98@purdue.edu>
2016-01-23 10:53:33 -0500

    add  in RoomCtrl and add joinRoom function

--- static/controllers/rooms.js
+++ static/controllers/rooms.js
@@ -1,5 +1,5 @@
-angular.module('techNodeApp').controller('RoomsCtrl', function($scope) {
-	angular.module('techNodeApp').controller('RoomsCtrl', function($scope, socket) {
+
+	angular.module('techNodeApp').controller('RoomsCtrl', function($scope, $location, $socket) {
 		socket.emit('getAllRooms')
 		scoket.on('roomsData', function(rooms) {
 			$scope.rooms = $scope._rooms = rooms
@@ -21,9 +21,27 @@ angular.module('techNodeApp').controller('RoomsCtrl', function($scope) {
 			})
 		}
 
+		$scope.enterRoom = function(room) {
+			scoket.emit('joinRoom', {
+				user: $scope.me,
+				room: room
+			})
+		}
+
+		$socket.once('joinRoom' + $scope.me._id, function(join) {
+			$location.path('/rooms/' + join.room._id)
+		})
+
+		socket.on('joinRoom', function(join) {
+			$scope.rooms.forEach(function(room) {
+				if (room._id == join.room._id) {
+					room.users.push(join.user)
+				}
+			})
+		})
+
 		socket.on('roomAdded', function(room) {
 			$scope._rooms.push(room)
 			$scope.searchRoom()
 		})
 	})
-})
